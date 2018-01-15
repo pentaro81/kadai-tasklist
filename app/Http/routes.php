d0b45318336c2c636ff715f6f23da3d63d999729
pentaro <penta_git@yahoo.co.jp>
2018-01-15 03:20:13 +0000

    CRUD tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks','TasksController');
+
+
+//Route::get('tasks/{id}','TasksController@show');
+//Route::post('tasks','TasksController@store');
+//Route::put('tasks/{id}','TasksController@update');
+//Route::delete('tasks/{id}','TasksController@destroy');
+
+//Route::get('tasks','TasksController@index');
+
+//Route::get('tasks/create','TasksController@create');
+
+//Route::get('tasks/{id}/edit','TasksController@edit')->name('tasks.edit');
+
+
